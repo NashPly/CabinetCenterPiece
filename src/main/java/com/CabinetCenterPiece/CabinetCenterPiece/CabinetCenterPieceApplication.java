7d1c81ccdab2f22c86a7852d0ec37e2cc9ab5155
NashPly <tbeals@nashvilleplywood.com>
2022-11-17 15:11:42 -0600

    init

---
+++ src/main/java/com/CabinetCenterPiece/CabinetCenterPiece/CabinetCenterPieceApplication.java
@@ -0,0 +1,13 @@
+package com.CabinetCenterPiece.CabinetCenterPiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetCenterPieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetCenterPieceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CabinetCenterPiece/CabinetCenterPiece/CabinetCenterPieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CabinetCenterPiece.CabinetCenterPiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetCenterPieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
